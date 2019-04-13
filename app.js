199cc22395ceb9567a6ad3d1a599ce520460fb30
Str1k3-r <prateektank120@gmail.com>
2019-04-13 00:27:14 -0400

    Initialized mongodb

--- app.js
+++ app.js
@@ -2,17 +2,37 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const {buildSchema} = require('graphql');
+const mongoose = require('mongoose');
+
+//Global vars
+const events = [];
 
 const app = express();
 app.use(express.json());
 app.use("/api", graphqlHTTP({
     schema: buildSchema(`
+    
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+        
         type RootQuery {
-            events: [String!]!
+            events: [Event!]!
         }
         
         type RootMutation {
-            createEvent(name: String): String
+            createEvent(eventInput: EventInput!): Event
         }
     
         schema {
@@ -26,12 +46,21 @@ app.use("/api", graphqlHTTP({
         },
 
         createEvent: (args) => {
-            const eventName = args.name;
-            return eventName;
+            const event = {
+                _id: Math.random().toString(),
+                title: args.eventInput.title,
+                description: args.eventInput.description,
+                price: +args.eventInput.price,
+                date: args.eventInput.date
+            };
+            events.push(event);
+            return event;
         }
     },
     graphiql: true
 }));
 
+
+mongoose.connect('https://localhost:27017/admin');
 
 app.listen(3000);
